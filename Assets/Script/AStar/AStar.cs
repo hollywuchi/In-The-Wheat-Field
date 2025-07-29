80265a46e0bc6a2964b2fd0777842abfaadf58a0
hollywuchi <154769557+hollywuchi@users.noreply.github.com>
2025-07-29 22:50:08 +0800

    路径创建成功，准备提交
    -- 实现A*算法的核心，实现从起点到终点的完整节点判断流程
    -- 逻辑依旧十分复杂，要下功夫研究

--- Assets/Script/AStar/AStar.cs
+++ Assets/Script/AStar/AStar.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using Farm.Map;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 namespace Farm.AStar
@@ -26,7 +29,7 @@ namespace Farm.AStar
             // 查找最短路径
             if (FindShortestPath())
             {
-                // 构建NPC移动路径
+                // TODO:构建NPC移动路径
             }
 
         }
@@ -83,7 +86,10 @@ namespace Farm.AStar
             return true;
         }
 
-
+        /// <summary>
+        /// 找到最短路径，将所有node添加到closeNodeList
+        /// </summary>
+        /// <returns></returns>
         private bool FindShortestPath()
         {
             // 添加起点
@@ -107,8 +113,80 @@ namespace Farm.AStar
                 }
 
                 // 计算周围8个节点补充到OpenList
+                EvaluateNeighbourNodes(closeNode);
             }
             return pathFound;
+        }
+
+        /// <summary>
+        /// 评估周围的8个点，并生成对应的消耗值
+        /// </summary>
+        /// <param name="currentNode"></param>
+        private void EvaluateNeighbourNodes(Node currentNode)
+        {
+            Vector2Int currentNodePos = currentNode.gridPosition;
+            Node validNeighbourNode;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    validNeighbourNode = GetValidNeighbourNode(currentNode.gridPosition.x + x, currentNode.gridPosition.y + y);
+
+                    if (validNeighbourNode != null)
+                    {
+                        if (!openNodeList.Contains(validNeighbourNode))
+                        {
+                            validNeighbourNode.gCost = currentNode.gCost + GetDistance(currentNode, validNeighbourNode);
+                            validNeighbourNode.hCost = GetDistance(validNeighbourNode, targetNode);
+                            // 链接父节点
+                            validNeighbourNode.parentNode = currentNode;
+                            openNodeList.Add(validNeighbourNode);                            
+                        }
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 找到有效的Node，非障碍，非已选择
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Node GetValidNeighbourNode(int x, int y)
+        {
+            if (x > gridWitch || y > gridHeight || x < 0 || y < 0)
+                return null;
+
+            Node neighbourNode = gridNodes.GetGridNode(x, y);
+
+            if (neighbourNode.isObstable || closedNodeList.Contains(neighbourNode))
+                return null;
+            else
+                return neighbourNode;
+        }
+
+        /// <summary>
+        /// 返回两点距离值
+        /// </summary>
+        /// <param name="nodeA"></param>
+        /// <param name="nodeB"></param>
+        /// <returns>14的倍数 + 10的倍数</returns>
+        private int GetDistance(Node nodeA, Node nodeB)
+        {
+            int xDistance = Mathf.Abs(nodeA.gridPosition.x - nodeB.gridPosition.x);
+            int yDistance = Mathf.Abs(nodeA.gridPosition.y - nodeB.gridPosition.y);
+
+            if (xDistance > yDistance)
+            {
+                return 14 * yDistance + 10 * (xDistance - yDistance);
+            }
+            return 14 * xDistance + 10 * (yDistance - xDistance);
         }
     }
 }
